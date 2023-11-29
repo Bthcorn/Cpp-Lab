074d39e08261d8bfc87d3e4bb74579a410a531f4
Bthcorn <139036880+Bthcorn@users.noreply.github.com>
2023-11-29 17:23:32 +0700

    lab1

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    std::cout << "This is my first program in C++" << std::endl;
+    std::cout << "Good bye!" << std::endl;
+};
